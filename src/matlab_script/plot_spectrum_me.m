9f00842e8179b64b5e95be132f71470361dee21a
aniccasora_yzu <aniccasora@yzu.edu.tw@yzu.edu.tw>
2022-01-20 01:56:28 +0800

    日常測試改參數

--- src/matlab_script/plot_spectrum_me.m
+++ src/matlab_script/plot_spectrum_me.m
@@ -15,13 +15,13 @@ exp_path = '.\exp_fig\';
 % school_windows='D:\git-repo'
 prj_path="D:\git-repo";  % 專案位置  //linux : /home/user/git-repo,  
 % ---------------------------------------------------------------
-timestamp_foldern='DAE_C_2022_0117_1238_28'; % log 下 時戳資料夾名
+timestamp_foldern='DAE_C_2022_0120_0150_52'; % log 下 時戳資料夾名
 %
 origin_path=fullfile(prj_path,'MFA_DAE_ME','src','dataset'); % 無損 source 根目錄
 % 重建後音源
 recons_path=fullfile(prj_path,'MFA_DAE_ME','src','log',timestamp_foldern,'test_reconstruct');
-s1_path=fullfile(prj_path,'MFA_DAE_ME','src','log',timestamp_foldern,'test_source1');
-s2_path=fullfile(prj_path,'MFA_DAE_ME','src','log',timestamp_foldern,'test_source2');
+s1_path=fullfile(prj_path,'MFA_DAE_ME','src','log',timestamp_foldern,'test_source1'); % test_my_source1, test_source1
+s2_path=fullfile(prj_path,'MFA_DAE_ME','src','log',timestamp_foldern,'test_source2'); % test_my_source2, test_source2
 s3_path=fullfile(prj_path,'MFA_DAE_ME','src','log',timestamp_foldern,'test_source3');
 lab_dir_name='';  % 比照 python 內的 lab_dir_name 資料夾變數，如沒設定可以為空
 
